e37ab863f8940cc84edd5749efdec1efffa79594
Jao-Dev <jpl71330@gmail.com>
2026-07-25 17:17:40 -0300

    feat: permitindo personagem ter uma imagem de avatar

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,14 @@
 class Character < ApplicationRecord
   audited
 
+  ACCEPTED_CONTENT_TYPES = [ "image/png", "image/jpeg" ].freeze
+
+  has_one_attached :avatar
+
   validates :name, :race, :ativo, presence: true
+  validates :avatar,
+    content_type: { in:
+    ACCEPTED_CONTENT_TYPES,
+    spoofin_protection: true
+    }
 end
--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -6,4 +6,24 @@ describe Character do
       it { is_expected.not_to have_valid(:race).when(nil, '') }
       it { is_expected.not_to have_valid(:ativo).when(nil, '') }
   end
+
+  describe 'validar imagem do avatar' do
+    it 'sendo imagem PNG' do
+      character = build(:character)
+      character.avatar.attach(
+        io: Rails.root.join('spec/resources/dummies/dummy.png').open,
+        filename: 'dummy.png'
+      )
+      expect(character).to be_valid
+    end
+
+    it 'sendo imagem HEIC' do
+      character = build(:character)
+      character.avatar.attach(
+        io: Rails.root.join('spec/resources/dummies/dummy.heic').open,
+        filename: 'dummy.png'
+      )
+      expect(character).not_to be_valid
+    end
+  end
 end
