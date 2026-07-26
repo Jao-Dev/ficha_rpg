a08fa97c0510864686ce72ed82d41ea485af8743
Jao-Dev <jpl71330@gmail.com>
2026-07-26 09:22:21 -0300

    Adicionando relacionamentos entre tabelas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_25_194719) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_26_113749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -73,10 +73,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_25_194719) do
 
   create_table "characters", force: :cascade do |t|
     t.boolean "ativo", default: true, null: false
+    t.bigint "campaign_id"
     t.datetime "created_at", null: false
     t.string "name", null: false
     t.string "race", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["campaign_id"], name: "index_characters_on_campaign_id"
+    t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -89,4 +93,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_25_194719) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "characters", "campaigns"
+  add_foreign_key "characters", "users"
 end
